9297ee509a19205cebf6c32332e1a52dce0a0ab4
Pedro Sousa <pedronsousa@gmail.com>
2021-10-07 14:03:36 +0100

    Add Development Team to xcode project

--- scripts/get_packages.sh
+++ scripts/get_packages.sh
@@ -14,5 +14,5 @@ rm -rf "${ROOT_DIR}"/packages
 mkdir "${ROOT_DIR}"/packages
 mkdir "${ROOT_DIR}"/packages/time_range_selector
 
-cp -r ~/Work/Projects/time_range_selector/lib "${ROOT_DIR}"/packages/time_range_selector
-cp -r ~/Work/Projects/time_range_selector/pubspec.* "${ROOT_DIR}"/packages/time_range_selector
+cp -r "${MY_PACKAGES}"/time-range-selector/lib "${ROOT_DIR}"/packages/time_range_selector
+cp -r "${MY_PACKAGES}"/time-range-selector/pubspec.* "${ROOT_DIR}"/packages/time_range_selector
